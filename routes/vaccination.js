4b8789cbaa44d7cfa5d8fc9397d0002ad116bd6b
viettu2k <doanviettu2k@gmail.com>
2021-09-27 09:31:45 +0700

    adding vaccination creat route

--- routes/vaccination.js
+++ routes/vaccination.js
@@ -2,8 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 const { create } = require("../controllers/vaccination");
-const { userSignupValidator } = require("../validator");
+const {
+    requireSignin,
+    isAuth,
+    isAdmin,
+    isMedicalStaff,
+} = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 
-router.post("/vaccination/create", create);
+router.post(
+    "/vaccinations/create/:userId",
+    requireSignin,
+    isAuth,
+    isMedicalStaff,
+    create
+);
+
+router.param("userId", userById);
 
 module.exports = router;
