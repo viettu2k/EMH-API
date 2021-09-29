7feb6bdeeb250fa71188709756a6c5e21348b1e6
viettu2k <doanviettu2k@gmail.com>
2021-09-29 20:29:06 +0700

    adding update user route

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -6,6 +7,8 @@ exports.userById = (req, res, next, id) => {
             return res.status(400).json({ error: "User not found" });
         }
         req.profile = user;
+        req.profile.hashed_password = undefined;
+        req.profile.salt = undefined;
         next();
     });
 };
@@ -19,4 +22,19 @@ exports.list = (req, res) => {
         }
         res.json(data);
     });
+};
+
+exports.update = (req, res) => {
+    User.findOneAndUpdate({ _id: req.profile._id }, { $set: req.body }, { new: true },
+        (err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "You are not authorized to perform this action",
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            res.json(user);
+        }
+    );
 };
--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { userById, list } = require("../controllers/user");
+const { userById, list, update } = require("../controllers/user");
 
 router.get("/users/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({
         user: req.profile,
     });
 });
+router.put("/users/:userId", requireSignin, isAuth, update);
 router.get("/users", list);
 
 router.param("userId", userById);
