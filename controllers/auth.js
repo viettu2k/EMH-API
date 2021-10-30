35c081fc005a1860dea798d938bc0be53fbee616
viettu2k <doanviettu2k@gmail.com>
2021-10-30 20:38:16 +0700

    refactoring

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,6 @@ const expressJwt = require("express-jwt"); // for authorization check
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
-    // console.log("req.body", req.body);
     const user = new User(req.body);
     user.save((err, user) => {
         if (err) {
@@ -12,8 +11,8 @@ exports.signup = (req, res) => {
                 err: errorHandler(err),
             });
         }
-        user.salt = undefined;
         user.hashed_password = undefined;
+        user.salt = undefined;
         res.json({
             user,
         });
@@ -29,19 +28,18 @@ exports.signin = (req, res) => {
                 .status(400)
                 .json({ err: "User with that email does not exist. Please signup" });
         }
-        // if user is found make sure email and password match
 
-        // create authentication method in user model
+        // if user is found make sure the email and password match
+        // create authenticate method in user model
         if (!user.authenticate(password)) {
-            return res.status(401).json({ error: "Email and password dont match" });
+            return res.status(401).json({
+                error: "Email and password dont match",
+            });
         }
-
         // generate a signed token with user id and secret
         const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-
-        // persist the token as 't' in cookie expiry date
+        // persist the token as "t" in cookie with expiry date
         res.cookie("t", token, { expire: new Date() + 9999 });
-
         // return response with user and token to frontend client
         const { _id, name, email, role } = user;
         return res.json({ token, user: { _id, email, name, role } });
@@ -63,7 +61,7 @@ exports.isAuth = (req, res, next) => {
     let user = req.profile && req.auth && req.profile._id == req.auth._id;
     if (!user) {
         return res.status(403).json({
-            error: "Access denied",
+            error: "Access denied!",
         });
     }
     next();
