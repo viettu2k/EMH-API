34e352475e7574f973d4c3b31cf0b51db3b57c98
viettu2k <doanviettu2k@gmail.com>
2021-11-27 10:47:52 +0700

    updating sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,7 +45,10 @@ exports.signin = (req, res) => {
         // persist the token as "t" in cookie with expiry date
         res.cookie("t", token, { expire: new Date() + 9999 });
         // return response with user and token to frontend client
-        // const { _id, name, email, role, dob,address,phoneNumbe } = user;
+        // const { _id, name, email, role, dob,address,phoneNumber } = user;
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        user.photo = undefined;
         return res.json({ token, user });
     });
 };
